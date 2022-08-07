c7405974e5a08076907027386c9f7e9901547aa1
nikolai <graf.nikolyai@gmail.com>
2022-08-07 17:10:53 +0400

    PHP_1_2.php

---
+++ PHP_1_2.php
@@ -0,0 +1,37 @@
+<?php
+//echo "В каком году произошло крешение Руси?\n",
+//    "Есть три варианта ответа \n";
+//
+//while(true){
+//echo ("a)810 \n");
+//echo ("b)990 \n");
+//echo ("c)740 \n");
+//$answap = readline("Ваш ответ: ");
+//
+//
+//if ($answap == 990 || $answap == "b") {
+//    echo("Вы ответили верно");
+//    break;
+//}elseif ($answap == 810 || $answap == "a" || $answap == "c" || $answap ==740) {
+//    echo "Вы ответили неверно ";
+//    break;
+//}echo ("Введите коректный ответ\n");
+//}
+
+
+//$affairs = (int)readline("Сколько у вас запланированных дел? ");
+//const TASK = ("Какая задача стоит перед вами сегодня? ");
+//const TIME = ("Сколько примерно времени эта задача займет? ");
+//$result = 0;
+//$out = "";
+//
+//for ($i = 0; $i < $affairs; $i++) {
+//    $task = "task$i";
+//    $time = "time$i";
+//    $$task = readline(TASK);
+//    $$time = (int)readline(TIME);
+//    $result += $$time;
+//    $out .= "- {$$task} ({$$time}ч) \n";
+//}
+//echo ($out);
+//echo "примерное время выполнеия плана = ({$result}ч) ";
